cb6276cfbe3eb9d07c42ad5bc4a96716a7c5dac0
Anurag Gupta <agupta@venacorp.com>
2025-05-14 18:30:17 +0530

    string -> objectId for userId

--- src/main/java/com/example/starter/model/User.java
+++ src/main/java/com/example/starter/model/User.java
@@ -1,12 +1,13 @@
 package com.example.starter.model;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="users")
 public class User {
     @Id
-    private String id;
+    private ObjectId id;
     private String name;
     private int age;
 
--- src/main/java/com/example/starter/repository/UserRepository.java
+++ src/main/java/com/example/starter/repository/UserRepository.java
@@ -1,13 +1,14 @@
 package com.example.starter.repository;
 
 import com.example.starter.model.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByName(String name);
 
     void deleteByName(String name);
